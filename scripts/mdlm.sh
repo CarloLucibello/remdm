ed7ecba95cda84c1eec4eab0ffe5f3bb996d2b36
CarloLucibello <carlo.lucibello@gmail.com>
2025-07-27 17:15:32 +0200

    updates

--- scripts/mdlm.sh
+++ scripts/mdlm.sh
@@ -1,43 +1,81 @@
 #!/bin/bash
-#SBATCH -J mdlm                       # Job name
-#SBATCH -o watch_folder/%x_%j.out     # log file (out & err)
-#SBATCH -N 1                          # Total number of nodes requested
-#SBATCH --get-user-env                # retrieve the users login environment
-#SBATCH --mem=32000                   # server memory requested (per node)
-#SBATCH -t 960:00:00                  # Time limit (hh:mm:ss)
-#SBATCH --partition=gpu               # Request partition
-#SBATCH --constraint="[3090|a5000|a6000|a100]"
-#SBATCH --ntasks-per-node=1
-#SBATCH --gres=gpu:1                  # Type/number of GPUs needed
-#SBATCH --open-mode=append            # Do not overwrite logs
-#SBATCH --requeue                     # Requeue upon preemption
+#SBATCH --job-name="mdlm"
+#SBATCH --time=24:00:00
+#SBATCH --partition=gpu
+# #SBATCH --qos=debug # only for debug_gpu and debug_cpu partitions
+#SBATCH --nodes=1
+#SBATCH --gpus=1               # num gpus. If set to 0 change the partition to defq or compute
+#SBATCH --cpus-per-task=8      # number of threads per task
+#SBATCH --ntasks=1             # SET EQUAL TO gpus IF DOING pytorch's DDP
+#SBATCH --output=out/%x_%j.out
+#SBATCH --error=err/%x_%j.err
+#SBATCH --mem-per-cpu=16000M  # memory per cpu core, default 8GB
 
-checkpoint_path=../outputs/checkpoints/mdlm.ckpt
+#SBATCH --account=lucibello 
+#SBATCH --mail-type=NONE   #notify for NONE, BEGIN, END, FAIL, REQUEUE, ALL
+#SBATCH --mail-user=carlo.lucibello@unibocconi.it
+
+## AVAILABLE PARTITIONS
+# defq, timelimit 3 days, Nodes=cnode0[1-4] (CPU)
+# compute, timelimit 15 days, Nodes=cnode0[5-8] (CPU)
+# long_gpu timelimit 3 days, Nodes=gnode0[1-2] (GPU)
+# gpu timelimit 1 day, Nodes=gnode0[1-4] (GPU)
+# medium_gpu, timelimit 3 hours, Nodes=gnode0[1-4] (GPU)
+# stata, timelimit 3 days, Nodes=cnode08 (CPU)
+# debug_cpu, timelimit 15 minutes, Nodes=cnode01 (short test on CPU)
+# debug_gpu, timelimit 15 minutes, Nodes=gnode0[1-4] (short test on GPU)
+
+## COMMON SLURM COMMANDS 
+# squeue 
+# sbatch job.sh
+# sinfo -Nel
+# scancel <job_id>
+
+module load miniconda3
+eval "$(conda shell.bash hook)"
+module load cuda/12.8
+
+conda activate remdm
+
+start_time=$(date +%s)
+
+
+SEED=1
+checkpoint_path=$HOME/Git/remdm/outputs/checkpoints/mdlm.ckpt
 T=0
-sampling_steps=1024
+sampling_steps=128 #1024
 p=0.9
-generated_seqs_path=../outputs/mdlm_T-${sampling_steps}_topp-${p}.json
+num_sample_batches=500 # 5000
+global_batch_size=512 # useless, since we set loader.batch_size=1 and loader.eval_batch_size=1
+batch_size=1        # if I try to increase this, I get OOM
+generated_seqs_path=$HOME/Git/remdm/outputs/mdlm_T-${sampling_steps}_topp-${p}_n-${num_sample_batches}.json
 
 export HYDRA_FULL_ERROR=1
 
 srun python -u -m main \
     mode=sample_eval \
-    loader.batch_size=1 \
-    loader.eval_batch_size=1 \
-    eval.perplexity_batch_size=1 \
     data=openwebtext-split \
+    data.cache_dir=$HOME/Git/remdm/outputs/data \
     model=small \
     parameterization=subs \
     backbone=dit \
     model.length=1024 \
     eval.checkpoint_path=${checkpoint_path} \
     time_conditioning=false \
-    +wandb.offline=true \
-    hydra.run.dir="${PWD}/outputs/mdlm" \
     T=${T} \
+    loader.global_batch_size=${global_batch_size} \
     sampling.steps=${sampling_steps} \
-    seed=1 \
-    sampling.num_sample_batches=5000 \
+    seed=${SEED} \
+    loader.batch_size=${batch_size} \
+    loader.eval_batch_size=${batch_size} \
+    eval.perplexity_batch_size=${batch_size} \
+    sampling.num_sample_batches=${num_sample_batches} \
     sampling.generated_seqs_path=${generated_seqs_path} \
     sampling.nucleus_p=${p} \
     sampling.sampler="mdlm"
+
+
+end_time=$(date +%s)
+elapsed_seconds=$((end_time - start_time))
+elapsed_hours=$(awk "BEGIN {printf \"%.4f\", $elapsed_seconds/3600}")
+echo "Elapsed time: $elapsed_hours hours"
